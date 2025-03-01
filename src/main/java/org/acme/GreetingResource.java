21e349887043fa12c408f898953a9243866279f3
stohirov <suxrobtoxirov001@gmail.com>
2025-03-01 15:12:17 +0500

    Initial setup

---
+++ src/main/java/org/acme/GreetingResource.java
@@ -0,0 +1,25 @@
+package org.acme;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import java.util.List;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello from Quarkus REST";
+    }
+
+    @GET()
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<String> custom() {
+        return List.of("String");
+    }
+
+}
